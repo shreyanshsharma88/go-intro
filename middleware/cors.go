4c8f9a9434345116a4c53a3379be6924bb0b0dfb
shreyanshSharma88 <shreyanshsharma480@gmail.com>
2024-12-15 13:06:51 +0530

    fix: cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,12 +1,20 @@
 package middleware
+
 import (
 	"net/http"
 )
+
 func CORSMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(res http.ResponseWriter, req *http.Request) {
 		res.Header().Set("Access-Control-Allow-Origin", "*")
-		res.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		res.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		res.Header().Set("Access-Control-Allow-Headers", "Content-Type, token")
+
+		if req.Method == http.MethodOptions {
+			res.WriteHeader(http.StatusOK)
+			return
+		}
+
 		next(res, req)
 	}
-} 
+}
